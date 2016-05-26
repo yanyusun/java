2012486b9d4c0cedaadf016ea2aa973661b1ab86
Yvan <xyx@iqingsou.com>
2016-05-26 14:01:14 +0800

    add atl.info.controller

---
+++ qs_auth/src/main/java/com/dqys/auth/controller/AtlInfoController.java
@@ -0,0 +1,11 @@
+package com.dqys.auth.controller;
+
+import com.dqys.core.base.BaseApiContorller;
+
+/**
+ * Created by Yvan on 16/5/26.
+ */
+
+
+public class AtlInfoController extends BaseApiContorller {
+}
