443ef3ace1b886aa9178a33112cb0f7b1faf5cc6
yanyusun <q64261750@163.com>
2016-07-17 14:46:20 +0800

    业务错误同一catch

--- qs_business_service/src/main/java/com/dqys/business/service/exception/handler/BusinessExceptionHandler.java
+++ qs_business_service/src/main/java/com/dqys/business/service/exception/handler/BusinessExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.dqys.business.service.exception.handler;
 
 import com.dqys.business.service.exception.bean.base.BussinessException;
+import com.dqys.core.constant.ResponseCodeEnum;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,7 +30,7 @@ public class BusinessExceptionHandler implements HandlerExceptionResolver {
     public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
         Map<String, Object> model = new HashMap<String, Object>();
         String msg = e.getMessage();
-        model.put("msg", msg);
+        model.put("exMsg", msg);
         String name = UNKOWN_EXCEPTION_NAME;
         int code = UNKOWN_ERROR;
         // 根据不同错误转向不同页面
@@ -36,9 +38,13 @@ public class BusinessExceptionHandler implements HandlerExceptionResolver {
             name = ((BussinessException) e).getExceptionName();
             code = ((BussinessException) e).getExceptionCode();
         }
-        model.put("name", name);
-        model.put("code", code);
+        model.put("exName", name);
+        model.put("exCode", code);
+        model.put("code", ResponseCodeEnum.BUSINESS_ERROR.getValue());
+        model.put("msg", ResponseCodeEnum.BUSINESS_ERROR.getText());
+        MappingJackson2JsonView jw = new MappingJackson2JsonView();
         LogManager.getLogger("businessErrorAsync").error("name:" + name + ",code:" + code + ",msg:" + msg);
-        return new ModelAndView("error", model);
+        jw.setAttributesMap(model);
+        return new ModelAndView(jw);
     }
 }
