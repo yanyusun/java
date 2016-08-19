37db55f0bec05d594a32db958143be4537e82eef
Pan <wangzhepan@163.com>
2016-08-19 09:25:33 +0800

    下载

--- qs_resource/src/main/java/com/dqys/resource/controller/ResourceController.java
+++ qs_resource/src/main/java/com/dqys/resource/controller/ResourceController.java
@@ -5,12 +5,18 @@ import com.dqys.core.constant.SysPropertyTypeEnum;
 import com.dqys.core.model.JsonResponse;
 import com.dqys.core.model.UserSession;
 import com.dqys.core.utils.*;
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -52,5 +58,21 @@ public class ResourceController {
         };
     }
 
+    @RequestMapping("/getSource")
+    public ResponseEntity<byte[]> getTmpSource(@RequestParam String fileName, boolean isTmp) {
+        File file = FileTool.getFile(fileName,isTmp);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Disposition", "attachment;filename="
+                + fileName);
+        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+        byte[] bytes =null;
+        try {
+            bytes = FileUtils.readFileToByteArray(file);
+        }catch (IOException e){
+            e.printStackTrace();
+            return new ResponseEntity(bytes, httpHeaders, HttpStatus.EXPECTATION_FAILED);
+        }
+        return new ResponseEntity(bytes, httpHeaders, HttpStatus.OK);
+    }
 
 }
