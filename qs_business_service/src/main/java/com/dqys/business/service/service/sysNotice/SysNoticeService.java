b02bc9133a972c511747a8ed6a1d6ae87320a04c
Pan <wangzhepan@163.com>
2016-08-25 17:35:42 +0800

    系统设置初始化

---
+++ qs_business_service/src/main/java/com/dqys/business/service/service/sysNotice/SysNoticeService.java
@@ -0,0 +1,7 @@
+package com.dqys.business.service.service.sysNotice;
+
+/**
+ * Created by yan on 16-8-25.
+ */
+public class SysNoticeService {
+}
---
+++ qs_business_service/src/main/java/com/dqys/business/service/service/sysNotice/impl/SysNoticeServiceImpl.java
@@ -0,0 +1,7 @@
+package com.dqys.business.service.service.sysNotice.impl;
+
+/**
+ * Created by yan on 16-8-25.
+ */
+public class SysNoticeServiceImpl {
+}
