c7cc21b8ce28c9c6b99fdb014fd69065085c2120
mkfeng <mkf@iqingsou.com>
2016-09-09 17:30:04 +0800

    修改借款人信息

--- qs_business_orm/src/main/java/com/dqys/business/orm/mapper/synthLender/SynthLenderMapper.java
+++ qs_business_orm/src/main/java/com/dqys/business/orm/mapper/synthLender/SynthLenderMapper.java
@@ -5,12 +5,23 @@ import com.dqys.business.orm.pojo.asset.PawnInfo;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by mkfeng on 2016/9/9.
  */
 public interface SynthLenderMapper {
 
-
+    /**
+     *查询处置借款人的所属人姓名
+     * @param objectType
+     * @param objectId
+     * @param status t_teammate_re表status
+     * @param typeTTR t_teammate_re表的type
+     * @param typeOUR object_user_relation表的type
+     * @return
+     */
+    Map getRealNameByOURelation(@Param("objectType") Integer objectType, @Param("objectId") Integer objectId,
+                                @Param("status") Integer status, @Param("typeTTR") Integer typeTTR, @Param("typeOUR") Integer typeOUR);
 
 }
--- qs_business_service/src/main/java/com/dqys/business/service/service/impl/SynthLenderServiceImpl.java
+++ qs_business_service/src/main/java/com/dqys/business/service/service/impl/SynthLenderServiceImpl.java
@@ -1,22 +1,23 @@
 package com.dqys.business.service.service.impl;
 
 import com.dqys.business.orm.constant.company.ObjectTypeEnum;
-import com.dqys.business.orm.mapper.asset.IOUInfoMapper;
-import com.dqys.business.orm.mapper.asset.LenderInfoMapper;
-import com.dqys.business.orm.mapper.asset.PawnInfoMapper;
+import com.dqys.business.orm.constant.coordinator.OURelationEnum;
+import com.dqys.business.orm.constant.coordinator.TeammateReEnum;
+import com.dqys.business.orm.mapper.asset.*;
 import com.dqys.business.orm.mapper.business.ObjectUserRelationMapper;
 import com.dqys.business.orm.mapper.synthLender.SynthLenderMapper;
-import com.dqys.business.orm.pojo.asset.IOUInfo;
-import com.dqys.business.orm.pojo.asset.LenderInfo;
-import com.dqys.business.orm.pojo.asset.PawnInfo;
+import com.dqys.business.orm.pojo.asset.*;
 import com.dqys.business.orm.pojo.business.ObjectUserRelation;
 import com.dqys.business.orm.query.business.ObjectUserRelationQuery;
+import com.dqys.business.service.constant.asset.ContactTypeEnum;
+import com.dqys.business.service.dto.asset.LenderSynDTO;
 import com.dqys.business.service.service.SynthLenderService;
 import com.dqys.business.service.utils.message.MessageUtils;
 import com.dqys.core.model.UserSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,13 +38,22 @@ public class SynthLenderServiceImpl implements SynthLenderService {
     private IOUInfoMapper iouInfoMapper;
     @Autowired
     private ObjectUserRelationMapper objectUserRelationMapper;
+    @Autowired
+    private ContactInfoMapper contactInfoMapper;
+    @Autowired
+    private AssetInfoMapper assetInfoMapper;
+
 
     @Override
     public Map pawnList(Integer lenderId) {
         Integer userId = UserSession.getCurrent() == null ? 0 : UserSession.getCurrent().getUserId();
         String userType = UserSession.getCurrent() == null ? "0" : UserSession.getCurrent().getUserType();
+        userId = 12;
+        userType = "31";
         Map<String, Object> map = new HashMap<String, Object>();
         LenderInfo lenderInfo = lenderInfoMapper.get(lenderId);//借款人信息
+        ContactInfo contactInfo = contactInfoMapper.getByModel(ObjectTypeEnum.LENDER.getValue().toString(), ContactTypeEnum.LENDER.getValue(), lenderId);
+        LenderSynDTO lenderSynDTO = transform(lenderInfo, contactInfo);//数据转换
         ObjectUserRelationQuery query = new ObjectUserRelationQuery();
         query.setObjectType(ObjectTypeEnum.LENDER.getValue());
         query.setObjectId(lenderId);
@@ -52,7 +62,7 @@ public class SynthLenderServiceImpl implements SynthLenderService {
         boolean flag = false;
         if (list.size() > 0) {
             ObjectUserRelation our = list.get(0);
-            if (our.getVisibleType()==1) {
+            if (our.getVisibleType() != null && our.getVisibleType() == 1) {
                 flag = true;
             }
         }
@@ -66,9 +76,52 @@ public class SynthLenderServiceImpl implements SynthLenderService {
             iouInfoList = iouInfoMapper.listByLenderId(lenderId);
         }
         map.put("result", "yes");
-        map.put("lenderInfo", lenderInfo);//借款人信息
+        map.put("lenderSynDTO", lenderSynDTO);//借款人信息
         map.put("pawnList", pawnInfoList);//抵押物信息列表
         map.put("iouList", iouInfoList);//借据信息列表
         return map;
+    }
+
+    private LenderSynDTO transform(LenderInfo lf, ContactInfo cf) {
+        LenderSynDTO dto = new LenderSynDTO();
+        if (cf != null) {
+            dto.setType(cf.getType());
+            dto.setAddress(cf.getAddress());
+            dto.setAvg(cf.getAvg());
+            dto.setName(cf.getName());
+            dto.setGender(cf.getGender().toString());
+        }
+        if (lf != null) {
+            dto.setLenderNo(lf.getLenderNo());
+            dto.setTags(lf.getTags());
+            dto.setAccrual(lf.getAccrual().toString());
+            dto.setLoan(lf.getLoan().toString());
+            dto.setAppraisal(lf.getAppraisal().toString());
+            dto.setLoanType(lf.getLoanType());
+            dto.setLoanMode(lf.getLoanMode());
+            dto.setLoanName(lf.getLoanName());
+            dto.setRealUrgeTime(lf.getRealUrgeTime());
+            dto.setPhoneUrgeTime(lf.getPhoneUrgeTime());
+            dto.setEntrustUrgeTime(lf.getEntrustUrgeTime());
+            dto.setFollowUpDate(lf.getFollowUpDate());
+            dto.setEnteringDate(lf.getCreateAt());
+            dto.setStartAt(lf.getStartAt());
+            dto.setEndAt(lf.getEndAt());
+            AssetInfo assetInfo = assetInfoMapper.get(lf.getAssetId());//获取资产包信息
+            if (assetInfo != null) {
+                dto.setAssetNo(assetInfo.getAssetNo());
+            }
+            long nowTime = lf.getEndAt().getTime() - new Date().getTime();
+            Integer day = (int) nowTime / (1000 * 3600 * 24);
+            dto.setSurplusDay(day.toString());
+            Map map = synthLenderMapper.getRealNameByOURelation(ObjectTypeEnum.LENDER.getValue(), lf.getId(),
+                    TeammateReEnum.STATUS_ACCEPT.getValue(), TeammateReEnum.TYPE_AUXILIARY.getValue(), OURelationEnum.TYPE_ALLOCATION_TEAM.getValue());
+            if (map != null) {
+                dto.setBelongId(MessageUtils.transMapToInt(map, "user_id"));
+                dto.setBelongName(MessageUtils.transMapToString(map, "real_name"));
+            }
+        }
+
+        return dto;
     }
 }
