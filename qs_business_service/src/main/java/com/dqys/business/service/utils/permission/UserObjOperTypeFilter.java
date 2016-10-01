221d843fe7d5d24117a67d9e1cb528b7b7a25140
Pan <wangzhepan@163.com>
2016-10-01 17:55:33 +0800

    权限控制

--- qs_business_service/src/main/java/com/dqys/business/service/utils/permission/UserObjOperTypeFilter.java
+++ qs_business_service/src/main/java/com/dqys/business/service/utils/permission/UserObjOperTypeFilter.java
@@ -3,13 +3,19 @@ package com.dqys.business.service.utils.permission;
 import com.dqys.business.orm.pojo.operType.OperType;
 import com.dqys.business.service.constant.ObjectEnum.UserInfoEnum;
 import com.dqys.core.constant.RoleTypeEnum;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by pan on 16-10-1.
  */
+@Service
+@Primary
 public class UserObjOperTypeFilter extends OperTypeFilter {
+
+
 
     private Integer objectType;
 
