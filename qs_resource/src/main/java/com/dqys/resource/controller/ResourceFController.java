36825550b8e610ce36dc04b402ce18d6fab77582
Pan <wangzhepan@163.com>
2016-10-24 15:05:48 +0800

    上传ｂｕｇ

--- qs_resource/src/main/java/com/dqys/resource/controller/ResourceFController.java
+++ qs_resource/src/main/java/com/dqys/resource/controller/ResourceFController.java
@@ -1,6 +1,8 @@
 package com.dqys.resource.controller;
 
 import com.dqys.core.utils.FileTool;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
@@ -13,8 +15,10 @@ import java.io.OutputStream;
  * 针对火狐没有缓存设计的文件预览
  * Created by yan on 16-10-24.
  */
-
+@Controller
+@RequestMapping("/resf")
 public class ResourceFController {
+    @RequestMapping("/getSource")
     public String getRes(HttpServletResponse response, @RequestParam String fileName, boolean isTmp) {
         File file = FileTool.getFile(fileName,isTmp);
 
