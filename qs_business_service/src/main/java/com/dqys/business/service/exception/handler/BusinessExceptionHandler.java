b7c7d341a98987e6ac3c719952be50efe5a2395c
Pan <wangzhepan@163.com>
2016-10-26 11:02:32 +0800

    修改异常返回格式

--- qs_business_service/src/main/java/com/dqys/business/service/exception/handler/BusinessExceptionHandler.java
+++ qs_business_service/src/main/java/com/dqys/business/service/exception/handler/BusinessExceptionHandler.java
@@ -26,25 +26,51 @@ public class BusinessExceptionHandler implements HandlerExceptionResolver {
      */
     public static int UNKOWN_ERROR = 9999;
 
+//    @Override
+//    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+//        Map<String, Object> model = new HashMap<String, Object>();
+//        String msg = e.getMessage();
+//        model.put("exMsg", msg);
+//        String name = UNKOWN_EXCEPTION_NAME;
+//        int code = UNKOWN_ERROR;
+//        // 根据不同错误转向不同页面
+//        if (e instanceof BussinessException) {
+//            name = ((BussinessException) e).getExceptionName();
+//            code = ((BussinessException) e).getExceptionCode();
+//        }
+//
+//
+//
+//
+//        model.put("exName", name);
+//        model.put("exCode", code);
+//        model.put("code", ResponseCodeEnum.BUSINESS_ERROR.getValue());
+//        model.put("msg", ResponseCodeEnum.BUSINESS_ERROR.getText());
+//        model.put("exception" ,e);
+//        MappingJackson2JsonView jw = new MappingJackson2JsonView();
+//        LogManager.getLogger("businessErrorAsync").error("name:" + name + ",code:" + code + ",msg:" + msg+"exception:"+e);
+//        jw.setAttributesMap(model);
+//        return new ModelAndView(jw);
+//    }
+
     @Override
     public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
         Map<String, Object> model = new HashMap<String, Object>();
+        model.put("exMsg", ResponseCodeEnum.BUSINESS_ERROR.getText());
+        model.put("exCode", ResponseCodeEnum.BUSINESS_ERROR.getValue());
         String msg = e.getMessage();
-        model.put("exMsg", msg);
+        model.put("msg",  e.getMessage());
         String name = UNKOWN_EXCEPTION_NAME;
         int code = UNKOWN_ERROR;
-        // 根据不同错误转向不同页面
         if (e instanceof BussinessException) {
             name = ((BussinessException) e).getExceptionName();
             code = ((BussinessException) e).getExceptionCode();
         }
-        model.put("exName", name);
-        model.put("exCode", code);
-        model.put("code", ResponseCodeEnum.BUSINESS_ERROR.getValue());
-        model.put("msg", ResponseCodeEnum.BUSINESS_ERROR.getText());
+        model.put("name",name);
+        model.put("code", code);
         model.put("exception" ,e);
         MappingJackson2JsonView jw = new MappingJackson2JsonView();
-        LogManager.getLogger("businessErrorAsync").error("name:" + name + ",code:" + code + ",msg:" + msg+"exception:"+e);
+        LogManager.getLogger("businessErrorAsync").error("name:" + name + ",code:" + code + ",msg:" + msg + "exception:" + e);
         jw.setAttributesMap(model);
         return new ModelAndView(jw);
     }
