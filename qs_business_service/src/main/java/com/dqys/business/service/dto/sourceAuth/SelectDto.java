af1e42e7a095883e5891efb3df94c1441f0386b4
Pan <wangzhepan@163.com>
2016-10-28 18:41:29 +0800

    ｓｏｕｒｃｅ＿ｉｎｉｔ

---
+++ qs_business_service/src/main/java/com/dqys/business/service/dto/sourceAuth/SelectDto.java
@@ -0,0 +1,27 @@
+package com.dqys.business.service.dto.sourceAuth;
+
+/**
+ * 资料实勘权限展示model
+ * Created by yan on 16-10-28.
+ */
+public class SelectDto {
+    private Integer id;
+
+    private String showName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getShowName() {
+        return showName;
+    }
+
+    public void setShowName(String showName) {
+        this.showName = showName;
+    }
+}
