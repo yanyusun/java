d956f0cd03f7892f098e6551d3810eca86f4f2e3
Pan <wangzhepan@163.com>
2016-10-28 19:01:04 +0800

    ｓｏｕｒｃｅ＿auth

--- qs_business_service/src/main/java/com/dqys/business/service/dto/sourceAuth/SelectDtoMap.java
+++ qs_business_service/src/main/java/com/dqys/business/service/dto/sourceAuth/SelectDtoMap.java
@@ -9,7 +9,31 @@ public class SelectDtoMap {
 
     private List<SelectDto> companyList;
 
-    private List<SelectDto> companyList;
+    private List<SelectDto> roleList;
 
-    private List<SelectDto> companyList;
+    private List<SelectDto> userList;
+
+    public List<SelectDto> getCompanyList() {
+        return companyList;
+    }
+
+    public void setCompanyList(List<SelectDto> companyList) {
+        this.companyList = companyList;
+    }
+
+    public List<SelectDto> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<SelectDto> roleList) {
+        this.roleList = roleList;
+    }
+
+    public List<SelectDto> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<SelectDto> userList) {
+        this.userList = userList;
+    }
 }
