f9f7783e574ed748938dc2ad10a759d7113e34e6
Pan <wangzhepan@163.com>
2016-11-01 15:42:07 +0800

    ｓｏｕｒｃｅ＿auth

--- qs_business_service/src/main/java/com/dqys/business/service/utils/common/NavUnviewServerAgent.java
+++ qs_business_service/src/main/java/com/dqys/business/service/utils/common/NavUnviewServerAgent.java
@@ -1,6 +1,7 @@
 package com.dqys.business.service.utils.common;
 
 import com.dqys.business.service.dto.sourceAuth.SelectDto;
+import com.dqys.business.service.service.common.NavUnviewService;
 
 import java.util.List;
 
@@ -9,8 +10,37 @@ import java.util.List;
  * Created by yan on 16-11-1.
  */
 public class NavUnviewServerAgent {
-    public List<SelectDto> getSelectDtoListed(){
+
+    private NavUnviewService navUnviewService;
+
+    public NavUnviewServerAgent(NavUnviewService navUnviewService) {
+        this.navUnviewService = navUnviewService;
+    }
+
+    /**
+     * 已选项
+     * @param navId 分类ｉｄ
+     * @return
+     */
+    public List<SelectDto> getSelectedDtoList(Integer navId){
         return null;
     };
-   // public List<>
+
+    /**
+     * 可选项
+     * @param navId 分类ｉｄ
+     * @return
+     */
+    public List<SelectDto> getSelectOptions(Integer navId){
+        return null;
+    }
+
+    /**
+     * 重新设置已选的内容
+     * @param navId
+     * @param list
+     */
+    public void reset(Integer navId,List<Integer> list){
+
+    }
 }
