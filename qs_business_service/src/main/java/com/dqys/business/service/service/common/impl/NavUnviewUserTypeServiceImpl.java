b4a94eceb2af294ae730930d61657941a5f2355e
Pan <wangzhepan@163.com>
2016-11-04 11:13:21 +0800

    auth

--- qs_business_service/src/main/java/com/dqys/business/service/service/common/impl/NavUnviewUserTypeServiceImpl.java
+++ qs_business_service/src/main/java/com/dqys/business/service/service/common/impl/NavUnviewUserTypeServiceImpl.java
@@ -10,6 +10,8 @@ import com.dqys.core.constant.NavUnviewEnum;
 import com.dqys.core.model.UserSession;
 import com.dqys.core.utils.NoSQLWithRedisTool;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,8 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by Administrator on 2016/11/1.
  */
+@Primary
+@Repository
 public class NavUnviewUserTypeServiceImpl implements NavUnviewUserTypeService {
 
     @Autowired
