899dea93f830dfbce51d1a480da1bd83a8164944
Pan <wangzhepan@163.com>
2016-11-09 16:44:56 +0800

    Merge branch 'source_auth' of http://114.215.239.181:6080/qs_plat/java into source_auth
    # Conflicts:
    #	qs_business_orm/src/main/java/com/dqys/business/orm/mapper/coordinator/CoordinatorMapper.java
    #	qs_business_orm/src/main/java/com/dqys/business/orm/mapper/coordinator/impl/CoordinatorMapperImpl.java
    #	qs_business_orm/src/main/resources/mybatis/mapper/coordinator/CoordinatorMapper.xml

--- qs_business_service/src/main/java/com/dqys/business/service/utils/common/NavUnviewServerAgent.java
+++ qs_business_service/src/main/java/com/dqys/business/service/utils/common/NavUnviewServerAgent.java
@@ -68,16 +68,18 @@ public class NavUnviewServerAgent {
             Iterator<SelectDto> intIter = selectDtoList.iterator();
             //移除父级关联的项,
             while (intIter.hasNext()) {
+                SelectDto selectDto = intIter.next();
+                int reId = selectDto.getReId().intValue();
                 //移除父拥有的
                 for (SelectDto parentUnview : allParentList) {
-                    if (intIter.next().getReId().intValue() == parentUnview.getReId()) {
+                    if (reId == parentUnview.getReId()) {
                         intIter.remove();
                         break;
                     }
                 }
                 //是子类的去除
                 for (SelectDto navUnview : navlist) {
-                    if (intIter.next().getReId().intValue() == navUnview.getReId()) {
+                    if (reId == navUnview.getReId()) {
                         intIter.remove();
                         break;
                     }
@@ -103,16 +105,18 @@ public class NavUnviewServerAgent {
             Iterator<SelectDto> intIter = selectOptionsList.iterator();
             //移除父级关联的项,
             while (intIter.hasNext()) {
+                SelectDto selectDto = intIter.next();
+                int reId = selectDto.getReId().intValue();
                 //移除父拥有的
                 for (SelectDto parentUnview : allParentList) {
-                    if (intIter.next().getReId().intValue() == parentUnview.getReId()) {
+                    if (reId == parentUnview.getReId()) {
                         intIter.remove();
                         break;
                     }
                 }
                 //是子类的就可见为false
                 for (SelectDto navUnview : navlist) {
-                    if (intIter.next().getReId().intValue() == navUnview.getReId()) {
+                    if (reId == navUnview.getReId()) {
                         intIter.next().setVisible(false);
                         break;
                     }
@@ -122,8 +126,8 @@ public class NavUnviewServerAgent {
         return selectOptionsList;
     }
 
-    public List<SelectDto> getUnview(Integer navId, Integer object, Integer objectId){
-        init(navId,object,objectId);//
+    public List<SelectDto> getUnview(Integer navId, Integer object, Integer objectId) {
+        init(navId, object, objectId);//
         List<SelectDto> list = new LinkedList<>();
         list.addAll(allParentList);
         list.addAll(navlist);
@@ -177,15 +181,15 @@ public class NavUnviewServerAgent {
                         break;
                     }
                 }
-                List<Integer> list=null;
+                List<Integer> list = null;
                 if (isInInitCache) {//如果在缓存里已经有值,将缓存中的其他值加入数据库
-                    list=NavUtil.selectDtosToIntegers(selectDtos);
+                    list = NavUtil.selectDtosToIntegers(selectDtos);
                     list.remove(reId);
                 } else {//将缓存中的值和当前的值加入数据库
-                    list=NavUtil.selectDtosToIntegers(selectDtos);
+                    list = NavUtil.selectDtosToIntegers(selectDtos);
                     list.add(reId);
                 }
-                navUnviewService.add(navId, object, objectId,list);
+                navUnviewService.add(navId, object, objectId, list);
             }
         }
 
@@ -220,6 +224,7 @@ public class NavUnviewServerAgent {
 
     /**
      * 初始化所有父不可见和当前不可见的内容
+     *
      * @param navId
      * @param object
      * @param objectId
