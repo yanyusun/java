3399518ebb3810eb6a664d18433093dbff65c053
mkfeng <mkf@iqingsou.com>
2016-11-11 16:04:54 +0800

    修改

--- qs_business_service/src/main/java/com/dqys/business/service/dto/company/DistributionDTO.java
+++ qs_business_service/src/main/java/com/dqys/business/service/dto/company/DistributionDTO.java
@@ -12,10 +12,54 @@ public class DistributionDTO {
     private Integer platformNum = 0; // 平台数量
     private Integer mechanismNum = 0; // 机构数量
     private Integer disposeNum = 0; // 处置方数量
+    private double accrual = 0; // 总利息'
+    private double loan = 0; // 总贷款
+    private double appraisal = 0; // 总评估
+    private String numberNo; //编号
+    private String name; // 名称
 
     private List<CompanyTeamReDTO> companyTeamReDTOList; // 分配器成员
     private List<BusinessServiceDTO> businessServiceDTOList; // 业务流成员
 
+    public double getAccrual() {
+        return accrual;
+    }
+
+    public void setAccrual(double accrual) {
+        this.accrual = accrual;
+    }
+
+    public double getLoan() {
+        return loan;
+    }
+
+    public void setLoan(double loan) {
+        this.loan = loan;
+    }
+
+    public double getAppraisal() {
+        return appraisal;
+    }
+
+    public void setAppraisal(double appraisal) {
+        this.appraisal = appraisal;
+    }
+
+    public String getNumberNo() {
+        return numberNo;
+    }
+
+    public void setNumberNo(String numberNo) {
+        this.numberNo = numberNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Integer getId() {
         return id;
