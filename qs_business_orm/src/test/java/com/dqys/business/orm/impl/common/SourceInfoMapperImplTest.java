7262625147c230f0b7333de1d26896a2b4cdcfaa
yanyusun <342088816@qq.com>
2016-11-14 01:53:39 +0800

    del test

--- qs_business_orm/src/test/java/com/dqys/business/orm/impl/common/SourceInfoMapperImplTest.java
+++ qs_business_orm/src/test/java/com/dqys/business/orm/impl/common/SourceInfoMapperImplTest.java
@@ -28,25 +28,25 @@ public class SourceInfoMapperImplTest extends BaseTest {
 
     @Test
     public void test() {
-        List<Integer> userTypes=new ArrayList<>();
-        userTypes.add(310);
-        userTypes.add(313);
-        userInfoMapper.insertSelectiveByUserInfo(43,userTypes, 11, 123);
-        SourceInfo sourceInfo = new SourceInfo();
-        sourceInfo.setNavId(1);
-        sourceInfo.setCode("code" + 1);
-        sourceInfo.setLenderId(1);
-        sourceInfo.setNavId(1);
-        sourceInfo.setShow(1);
-        sourceInfo.setWatermark(1);
-
-        Integer result = sourceInfoMapper.insert(sourceInfo);
-        Assert.assertNotNull(result);
-
-        Integer id = sourceInfo.getId();
-        SourceInfo sourceInfo1 = sourceInfoMapper.get(id);
-        Assert.assertNotNull(sourceInfo1);
-        Assert.assertEquals(sourceInfo1.getCode(), sourceInfo.getCode());
+//        List<Integer> userTypes=new ArrayList<>();
+//        userTypes.add(310);
+//        userTypes.add(313);
+//        userInfoMapper.insertSelectiveByUserInfo(43,userTypes, 11, 123);
+//        SourceInfo sourceInfo = new SourceInfo();
+//        sourceInfo.setNavId(1);
+//        sourceInfo.setCode("code" + 1);
+//        sourceInfo.setLenderId(1);
+//        sourceInfo.setNavId(1);
+//        sourceInfo.setShow(1);
+//        sourceInfo.setWatermark(1);
+//
+//        Integer result = sourceInfoMapper.insert(sourceInfo);
+//        Assert.assertNotNull(result);
+//
+//        Integer id = sourceInfo.getId();
+//        SourceInfo sourceInfo1 = sourceInfoMapper.get(id);
+//        Assert.assertNotNull(sourceInfo1);
+//        Assert.assertEquals(sourceInfo1.getCode(), sourceInfo.getCode());
 
 
     }
