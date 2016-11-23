46ddacda2e623c5a91cf3b7a50a8412bf68188e4
Pan <wangzhepan@163.com>
2016-11-23 17:59:41 +0800

    文案修改

--- qs_business_service/src/main/java/com/dqys/business/service/constant/ObjectEnum/UserInfoEnum.java
+++ qs_business_service/src/main/java/com/dqys/business/service/constant/ObjectEnum/UserInfoEnum.java
@@ -12,18 +12,18 @@ public enum UserInfoEnum {
     LOOK_USER(995, "查看用户列表"),
     DEL_USER(996, "删除用户"),
 
-    COORDINATOR_ADD_USER(9901, "协作添加用户"),
-    COORDINATOR_DEL_USER(9902, "协作删除用户"),
-    COORDINATOR_REPLACE_USER(9903, "协作替换用户"),
-    COORDINATOR_ACCEPT_ADD_USER(9904, "同意被添加进协作"),
-    COORDINATOR_REJECT_ADD_USER(9905, "拒绝被添加进协作"),
+    COORDINATOR_ADD_USER(9901, "案组添加用户"),
+    COORDINATOR_DEL_USER(9902, "案组删除用户"),
+    COORDINATOR_REPLACE_USER(9903, "案组替换用户"),
+    COORDINATOR_ACCEPT_ADD_USER(9904, "同意被添加进案组"),
+    COORDINATOR_REJECT_ADD_USER(9905, "拒绝被添加进案组"),
 
-    DISTRIBUTION_ADD_THEIR(9906, "分配添加所属人"),
-    DISTRIBUTION_DEL_THEIR(9907, "分配删除所属人"),
-    DISTRIBUTION_ADD_GENERAL(9908, "分配添加业务流转用户"),
-    DISTRIBUTION_DEL_GENERAL(9909, "分配删除业务流转用户"),
-    DISTRIBUTION_ACCEPT_ADD(99010, "同意添加进分配器"),
-    DISTRIBUTION_REJECT_DEL(99011, "拒绝添加进分配器"),
+    DISTRIBUTION_ADD_THEIR(9906, "清收案组添加所属人"),
+    DISTRIBUTION_DEL_THEIR(9907, "清收案组删除所属人"),
+    DISTRIBUTION_ADD_GENERAL(9908, "清收案组添加用户"),//清收案组添加业务流转用户
+    DISTRIBUTION_DEL_GENERAL(9909, "清收案组删除用户"),//清收案组删除业务流转用户
+    DISTRIBUTION_ACCEPT_ADD(99010, "同意添加进清收案组"),
+    DISTRIBUTION_REJECT_DEL(99011, "拒绝添加进清收案组"),
 
 
     USER_TYPE_COMMON(0, "普通员工"),
