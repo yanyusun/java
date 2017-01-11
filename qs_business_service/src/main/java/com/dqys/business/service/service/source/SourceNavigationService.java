30e734a2107d26d18698126b96bec7de522b487d
Pan <wangzhepan@163.com>
2017-01-11 13:38:39 +0800

    Merge branch 'qs_sale' of http://114.215.239.181:6080/qs_plat/java into qs_sale
    # Conflicts:
    #	qs_sale_service/src/main/java/com/dqys/sale/service/impl/FixedAssetServiceImpl.java
    #	qs_sale_service/src/main/java/com/dqys/sale/service/impl/UserBondServiceImpl.java

---
+++ qs_business_service/src/main/java/com/dqys/business/service/service/source/SourceNavigationService.java
@@ -0,0 +1,8 @@
+package com.dqys.business.service.service.source;
+
+/**
+ * Created by pan on 17-1-11.
+ */
+public interface SourceNavigationService {
+    Integer addNavigation();
+}
---
+++ qs_business_service/src/main/java/com/dqys/business/service/service/source/impl/SourceNavigationServiceImpl.java
@@ -0,0 +1,23 @@
+package com.dqys.business.service.service.source.impl;
+
+import com.dqys.business.orm.mapper.common.SourceNavigationMapper;
+import com.dqys.business.service.service.source.SourceNavigationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by yan on 17-1-11.
+ */
+@Primary
+@Repository
+public class SourceNavigationServiceImpl implements SourceNavigationService {
+
+    @Autowired
+    private SourceNavigationMapper sourceNavigationMapper;
+
+    @Override
+    public Integer addNavigation() {
+        return null;
+    }
+}
